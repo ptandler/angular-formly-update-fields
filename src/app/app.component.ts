63b143eef0eb7d0be9afe93c798132d9d97e2b25
Peter Tandler <p.github@tandlers.de>
2024-01-15 15:08:59 +0100

    wip

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
+import {FormGroup} from "@angular/forms";
+import {FormlyFieldConfig} from "@ngx-formly/core";
 
 @Component({
   selector: 'app-root',
@@ -6,5 +8,65 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'angular-formly-update-fields';
+  form = new FormGroup({});
+  model = {};
+  fields: FormlyFieldConfig[] = [
+    {
+      key: 'input',
+      type: 'input',
+      props: {
+        label: 'Input',
+        placeholder: 'Input placeholder',
+        required: true,
+      },
+    },
+    {
+      key: 'textarea',
+      type: 'textarea',
+      props: {
+        label: 'Textarea',
+        placeholder: 'Textarea placeholder',
+        required: true,
+      },
+    },
+    {
+      key: 'checkbox',
+      type: 'checkbox',
+      props: {
+        label: 'Checkbox',
+      },
+    },
+    {
+      key: 'select',
+      type: 'select',
+      props: {
+        label: 'Select',
+        placeholder: 'Select placeholder',
+        required: true,
+        options: [
+          {label: 'Option 1', value: '1'},
+          {label: 'Option 2', value: '2'},
+          {label: 'Option 3', value: '3'},
+        ],
+      },
+    },
+    {
+      key: 'radio',
+      type: 'radio',
+      props: {
+        label: 'Radio',
+        required: true,
+        options: [
+          {label: 'Option 1', value: '1'},
+          {label: 'Option 2', value: '2'},
+        ],
+      },
+    },
+  ];
+
+  onSubmit() {
+    if (this.form.valid) {
+      alert(JSON.stringify(this.model, null, 2));
+    }
+  }
 }
--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,14 @@
-import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+import {bootstrapApplication} from "@angular/platform-browser";
+import {AppComponent} from "./app/app.component";
+import {provideAnimations} from "@angular/platform-browser/animations";
+import {platformBrowserDynamic} from "@angular/platform-browser-dynamic";
+import {AppModule} from "./app/app.module";
 
-import { AppModule } from './app/app.module';
-
+/*
+bootstrapApplication(AppComponent,
+  {providers: [provideAnimations()]})
+  .catch(err => console.error(err));
+*/
 
 platformBrowserDynamic().bootstrapModule(AppModule)
   .catch(err => console.error(err));
